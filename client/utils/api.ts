f06130822f0b65d4b26b40d4c776b1811f38e488
Deepak Nagar <deepnngarr@gmail.com>
2025-12-20 15:05:11 +0530

    Prepared API for production

--- client/utils/api.ts
+++ client/utils/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', // Pointing to your new Port 8000
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api',
   headers: {
     'Content-Type': 'application/json',
   },
